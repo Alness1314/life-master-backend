5ccd13b70ca79f300c9e8ad1a1e4e9ca2874d557
Alberto <alberto.ordaz@susess.com>
2025-07-14 17:51:59 -0600

    feat: :zap: mod swaggers item server

--- src/main/java/com/alness/lifemaster/swagger/SwaggerConfig.java
+++ src/main/java/com/alness/lifemaster/swagger/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.alness.lifemaster.swagger;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,11 +18,15 @@ public class SwaggerConfig {
 	private static final String SCHEME = "Bearer";
 	private static final String BEARER_FORMAT = "JWT";
 
+	@Value("${swg.server.prod}")
+    private String serverProd;
+
 	@Bean
 	public OpenAPI customOpenAPI() {
 		return new OpenAPI()
 				.info(getInfo())
 				.addServersItem(new Server().url("http://localhost:7091").description("Development Server"))
+				.addServersItem(new Server().url(serverProd).description("Prod Server"))
 				.components(createComponents())
 				.addSecurityItem(new SecurityRequirement().addList(SCHEME_NAME));
 	}
